8ec065d87c0d323ba3180ed9482b8f7b1769c0a4
Sandrakh  Yikwa <141068388+drakeDjkw@users.noreply.github.com>
2024-07-02 02:19:27 +0800

    Kaggle Notebook | Exercise: Syntax, Variables, and Numbers | python

---
+++ audio_filters/__init__.py
@@ -0,0 +1,68 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d686dae6",
+   "metadata": {
+    "_cell_guid": "b96bc7d7-57d2-4dd7-922a-3ea64b005476",
+    "_uuid": "aa303c56-6589-4b7b-b5e3-ee4f00d9fbba",
+    "collapsed": false,
+    "jupyter": {
+     "outputs_hidden": false
+    },
+    "papermill": {
+     "duration": 0.001851,
+     "end_time": "2024-07-01T18:19:15.166301",
+     "exception": false,
+     "start_time": "2024-07-01T18:19:15.164450",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "isGpuEnabled": false,
+   "isInternetEnabled": false,
+   "language": "python",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.13"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.341809,
+   "end_time": "2024-07-01T18:19:15.588800",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2024-07-01T18:19:12.246991",
+   "version": "2.5.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
